73667e0a522c9ef957e5e9106a9fdd2529ae56fc
ZhangedMyself <58993406+ZhangedMyself@users.noreply.github.com>
2019-12-17 14:40:40 -0500

    Update Mammal.java

--- Mammal.java
+++ Mammal.java
@@ -1,23 +1,31 @@
-abstract class Mammal{
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.awt.Graphics;
 
-  public int xCord;
-  public int yCord;
-  public int health;
-  public int damage;
-  public int speed;
+abstract class Mammal {
+
+  public BufferedImage sprite;
+  public int xCord, yCord, xDirection, yDirection, eWidth, eHeight, health, damage, speed;
+  public Rectangle hitbox;
 
   //constructor 
-  Mammal(int xCord, int yCord, int health, int damage, int speed){
+  Mammal(int xCord, int yCord, int health, int damage, int speed) {
     this.xCord = xCord;
     this.yCord = yCord;
+    this.xDirection = 0;
+    this.yDirection = 0;
     this.health = health;
     this.damage = damage;
     this.speed = speed;
   }
   
   //move method
+  public abstract void loadSprite();
+  
+  public abstract void draw(Graphics g);
+  
   public abstract void move();
   
   public abstract void attack();
-  
 }
