059cf3df92e8312a5737da233cdfc4b80c0b0931
Meow157 <57512426+MiaoE@users.noreply.github.com>
2019-12-17 22:24:01 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
+import java.util.Random;
 public class Main {
   public static void main(String[] args) {
+    Random random = new Random();
 
+    int[][] map = new int[25][25];
+    for(int i = 0; i < 25; i++) {
+      for(int j = 0; j < 25; j++) {
+        int choice = random.nextInt(100)+1;
+        if(choice < 95) map[i][j] = 1;
+        else map[i][j] = 2;//doesnt work
+      }
+    }
+
+    Map panel = new Map("Our Game", map);
+    while(true) {
+      panel.refresh();
+    }
   }
 }
