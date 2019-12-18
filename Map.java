31181345377b533370ac25711c6ccb8185515a20
Meow157 <57512426+MiaoE@users.noreply.github.com>
2019-12-17 22:24:34 -0500

    Update Map.java

--- Map.java
+++ Map.java
@@ -1,9 +1,3 @@
-/**
- * This template can be used as reference or a starting point
- * for your final summative project
- * @author Mangat
- **/
-
 //Graphics &GUI imports
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -22,18 +16,21 @@ import java.awt.event.MouseEvent;
 class Map extends JFrame {
 
   //class variable (non-static)
+  static int GridToScreenRatio;
   static double x, y;
+  private int[][] map;
   static GamePanel gamePanel;
 
-  Map() {
+  Map(String title, int[][] map) {
 
-    super("My Game");
+    super(title);
+    map = map;
     // Set the frame to full screen 
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
     // this.setUndecorated(true);  //Set to true to remove title bar
     //frame.setResizable(false);
-    
+    GridToScreenRatio = (map.length) / (map[0].length + 1);
     gamePanel = new GamePanel();
     this.add(new GamePanel());
 
@@ -53,6 +50,10 @@ class Map extends JFrame {
 
   }
 
+  public void refresh() {
+    this.repaint();
+  }
+
   //the main gameloop - this is where the game state is updated
   public void animate() {
 
@@ -64,6 +65,7 @@ class Map extends JFrame {
     }
   }
 
+
   /** --------- INNER CLASSES ------------- **/
 
   // Inner class for the the game area - This is where all the drawing of the screen occurs
@@ -74,6 +76,17 @@ class Map extends JFrame {
       g.setColor(Color.BLUE); //There are many graphics commands that Java can use
       g.fillRect((int)x, (int)y, 50, 50); //notice the x,y variables that we control from our animate method      
 
+      for(int i = 0; i < 25; i++) {
+        for(int j = 0; j < 25; j++) {
+          if(map[i][j] == 1) {
+            g.setColor(Color.green);
+            g.fillRect(j * GridToScreenRatio, i * GridToScreenRatio, GridToScreenRatio, GridToScreenRatio);
+          } else if(map[i][j] == 2) {
+            g.setColor(Color.cyan);
+            g.fillRect(j * GridToScreenRatio, i * GridToScreenRatio, GridToScreenRatio, GridToScreenRatio);
+          }
+        }
+      }
     }
   }
 
@@ -86,10 +99,14 @@ class Map extends JFrame {
     public void keyPressed(KeyEvent e) {
       //System.out.println("keyPressed="+KeyEvent.getKeyText(e.getKeyCode()));
 
-      if (KeyEvent.getKeyText(e.getKeyCode()).equals("D")) {  //If 'D' is pressed
+      if (KeyEvent.getKeyText(e.getKeyCode()).equals("A")) {  //If 'A' is pressed
+        System.out.println("YIKES A KEY!");
+      } else if (KeyEvent.getKeyText(e.getKeyCode()).equals("S")) {  //If 'S' is pressed
+        System.out.println("YIKES S KEY!");
+      } else if (KeyEvent.getKeyText(e.getKeyCode()).equals("D")) {  //If 'D' is pressed
         System.out.println("YIKES D KEY!");
-      } else if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {  //If ESC is pressed
-        System.out.println("YIKES ESCAPE KEY!"); //close frame & quit
+      } else if (KeyEvent.getKeyText(e.getKeyCode()).equals("W")) {  //If 'W' is pressed
+        System.out.println("YIKES W KEY!");
       }
     }
 
