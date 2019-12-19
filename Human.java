1252f3e27bf205f3bb98e07b236a046032cad102
Kyle-Zhou <51883176+Kyle-Zhou@users.noreply.github.com>
2019-12-18 20:47:46 -0500

    Update Human.java

--- Human.java
+++ Human.java
@@ -1,20 +1,45 @@
-class Human extends Mammal{
+//NOT COMPLETE//
 
-  Human(int xCord, int yCord, int health, int damage, int speed){
+import java.awt.image.BufferedImage;
+import java.awt.Rectangle;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.awt.Graphics;
+import java.awt.Color;
+
+class Human extends Mammal {
+  BufferedImage sprite;
+  Human(int xCord, int yCord, int health, int damage, int speed) {
     super(xCord, yCord, health, damage, speed);
   }
 
+  public void loadSprite() {
+    try {
+      sprite = ImageIO.read(new File("q.png"));
+    } catch(Exception e) { System.out.println("Error Loading Sprite...");};
+  }
+  
+  public void spawn(Graphics g) {
+    g.setColor(Color.RED);
+    g.drawRect(50, 50, 15, 15);
+  }
+  
+
   @Override
-  public void move(){
-    System.out.print("pass");
+  public void move() {
+    hitbox.setLocation((int)hitbox.getX() + this.xDirection, (int)hitbox.getY() + this.yDirection);
+    this.xCord += this.xDirection;
+    this.yCord += this.yDirection;
   }
   
-  public void attack(){
+  public void up(){
+    this.xCord += 1;
+    System.out.print(xCord);
+  }
+  
+  public void attack() {
     
-  }
-  
-  public int swapGun(){
-    return 0;
   }
   
 }
