42c735c7932ec1d243f741fac291e4da569850ad
ZhangedMyself <58993406+ZhangedMyself@users.noreply.github.com>
2019-12-24 00:12:11 -0500

    Add files via upload

---
+++ Version 2019.12.24/Camera.java
@@ -0,0 +1,21 @@
+class Camera {
+  private static float xCamera, yCamera;
+  
+  Camera(float xCamera, float yCamera) {
+    this.xCamera = xCamera;
+    this.yCamera = yCamera;
+  }
+  
+  public void follow(Human duber, int xAdjust, int yAdjust) {
+    xCamera = -duber.xCentre + xAdjust;
+    yCamera = -duber.yCentre + yAdjust;
+  }
+  
+  public float getXCamera() {
+    return xCamera;
+  }
+  
+  public float getYCamera() {
+    return yCamera;
+  }
+}
