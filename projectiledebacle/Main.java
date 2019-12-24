9002bb0707acaf13703716bf81f3cb8d163cc5ac
Kyle-Zhou <51883176+Kyle-Zhou@users.noreply.github.com>
2019-12-24 00:04:02 -0500

    Delete Main.java

--- projectiledebacle/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    Game game = new Game("Duber's Revenge");
-    while(true) {
-      game.refresh();
-      try{ Thread.sleep(5000);} catch (Exception exc){}  //delay
-    }
-  }
-}
