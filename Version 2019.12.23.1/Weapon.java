c71e037f7f16b7cb3b25024e2e819211c7968863
ZhangedMyself <58993406+ZhangedMyself@users.noreply.github.com>
2019-12-26 22:56:39 -0500

    Delete Weapon.java

--- Version 2019.12.23.1/Weapon.java
+++
@@ -1,9 +0,0 @@
-abstract class Weapon {
-  public int xCord, yCord, damage;
-  
-  Weapon(int xCord, int yCord, int damage) {
-    this.xCord = xCord;
-    this.yCord = yCord;
-    this.damage = damage;
-  }
-}
