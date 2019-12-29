46d7ba3320a8279681e9a8c5d3f98f0c6a3446a0
ZhangedMyself <58993406+ZhangedMyself@users.noreply.github.com>
2019-12-29 00:26:50 -0500

    Add files via upload

---
+++ Version 2019.12.28.1/Cash.java
@@ -0,0 +1,38 @@
+import java.awt.image.BufferedImage;
+import java.awt.Rectangle;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.awt.Graphics;
+import java.awt.Color;
+
+class Cash extends Entity{
+  
+  private BufferedImage sprite;
+  
+  Cash(int xCord, int yCord){
+    super(xCord, yCord);
+    loadSprite();
+    super.seteWidth(sprite.getWidth());
+    super.seteHeight(sprite.getHeight());
+    super.setHitbox(new Rectangle(xCord, yCord, super.geteWidth(), super.geteHeight()));
+  }
+  
+  public void loadSprite() {
+    try {
+      sprite = ImageIO.read(new File("cash.png"));
+    } catch(Exception e) { System.out.println("Error Loading Sprite...");};
+  }
+  
+  public void draw(Graphics g) {
+    
+    g.drawImage(sprite, super.getxCord(), super.getyCord(), null);
+    g.setColor(Color.RED);
+    g.drawRect(super.getxCord(), super.getyCord(), super.geteWidth(), super.geteHeight());
+    
+//    g.drawImage(sprite, xCord, yCord+3, null);
+//    g.setColor(Color.RED);
+//    g.drawRect(xCord, yCord+3, eWidth, eHeight);
+    
+  }
+  
+}
---
+++ Version 2019.12.28.1/Map.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+class Map {
+
+  //class variable (non-static)
+  static double x, y;
+  private int[][] map = new int[75][75];
+  
+  Map() {
+    Random random = new Random();
+
+    for(int i = 0; i < 75; i++) {
+      for(int j = 0; j < 75; j++) {
+        int choice = random.nextInt(100)+1;
+        if(choice < 90) {
+          map[i][j] = 1;
+        } else if ((choice >= 90) && (choice < 95)) {
+          map[i][j] = 2;
+        } else {
+          map[i][j] = 3;
+        }
+      }
+    }
+  }
+  
+  public int[][] getMap(){
+    return map;
+  }
+  
+}
