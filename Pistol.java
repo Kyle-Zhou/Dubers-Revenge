52a0dabc8ce6da8b4faddbdd38f50da893418de9
Kyle-Zhou <51883176+Kyle-Zhou@users.noreply.github.com>
2019-12-31 12:25:08 -0500

    Add files via upload

---
+++ Pistol.java
@@ -0,0 +1,17 @@
+import javax.imageio.ImageIO;
+import java.io.File;
+
+class Pistol extends Weapon { 
+  
+  Pistol(int xCord, int yCord, int damage, int totalAmmo, int clip){
+    super(xCord, yCord, damage, totalAmmo, clip);
+    this.setDamage(damage);
+    this.setTotalAmmo(totalAmmo);
+    this.setClip(clip);
+  }
+  
+  public void reload(){
+    
+  }
+  
+}
