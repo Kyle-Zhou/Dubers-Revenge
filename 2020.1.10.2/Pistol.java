ae03dfffa89451ce0fbd606a7a513446361b3fc6
Kyle-Zhou <51883176+Kyle-Zhou@users.noreply.github.com>
2020-01-11 16:28:03 -0500

    Update Pistol.java

--- 2020.1.10.2/Pistol.java
+++ 2020.1.10.2/Pistol.java
@@ -2,13 +2,22 @@ class Pistol extends Weapon {
   
   Pistol(int xCord, int yCord, int damage, int totalAmmo, int clip){
     super(xCord, yCord, damage, totalAmmo, clip);
-    this.setDamage(damage);
-    this.setTotalAmmo(totalAmmo);
-    this.setClip(clip);
   }
   
   public void reload(){
-    
+    if ((getClip() < getOriginalClip()) && (getTotalAmmo() > 0)){
+
+      if (getTotalAmmo()+getClip() >= getOriginalClip()){
+        setTotalAmmo(getTotalAmmo()-(getOriginalClip()-getClip()));
+        setClip(getClip()+(getOriginalClip()-getClip()));
+        if (getTotalAmmo() < 0){
+          setTotalAmmo(0);
+        }
+      } else {
+        setClip(getClip()+getTotalAmmo());
+        setTotalAmmo(0);
+      }
+    }
   }
-  
+    
 }
