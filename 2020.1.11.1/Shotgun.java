59939cd6304b74ddd2d057f557f9ae9f48d399e7
ZhangedMyself <58993406+ZhangedMyself@users.noreply.github.com>
2020-01-12 02:51:11 -0500

    Update Shotgun.java

--- 2020.1.11.1/Shotgun.java
+++ 2020.1.11.1/Shotgun.java
@@ -4,20 +4,39 @@ import java.io.File;
 import java.awt.Graphics;
 
 class Shotgun extends Weapon {
+  private BufferedImage sheet, spriteRight, spriteLeft;
   
   Shotgun(int xCord, int yCord, int damage, int totalAmmo, int clip){
     super(xCord, yCord, damage, totalAmmo, clip);
+    loadSprites();
+    setDirection(true);
+  }
+  
+  public void loadSprites() {
+    try {
+      sheet = ImageIO.read(new File("shotgunSheet.png"));
+      spriteRight = sheet.getSubimage(0, 0, 68, 18);
+      spriteLeft = sheet.getSubimage(68, 0, 68, 18);
+    } catch(Exception e) {System.out.println("Error Loading Sprites...");};
   }
   
   public void draw(Graphics g) {
+     if (getDirection() == true) {
+      g.drawImage(spriteRight, getxCord(), getyCord(), null);
+    } else {
+      g.drawImage(spriteLeft, getxCord(), getyCord(), null);
+    }
   }
   
   public void move(Human duber) {
+    if (duber.getMoveLock() == false){
+      setxCord(getxCord() + duber.getxDirection());
+      setyCord(getyCord() + duber.getyDirection());
+    }
   }
   
   public void reload(){
     if ((getClip() < getOriginalClip()) && (getTotalAmmo() > 0)){
-
       if (getTotalAmmo()+getClip() >= getOriginalClip()){
         setTotalAmmo(getTotalAmmo()-(getOriginalClip()-getClip()));
         setClip(getClip()+(getOriginalClip()-getClip()));
