ab7ed1ada751460bb4b60ec77f123e5e282752a1
Meow157 <57512426+MiaoE@users.noreply.github.com>
2020-01-18 20:01:33 -0500

    Add files via upload

---
+++ 2020.1.18.1/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+  public static boolean menu = true, running = true, end = false;
+  public static Game game;
+
+  public static void main(String[] args) {
+
+    while (true) {
+      Menu mainmenu = new Menu();
+      while (menu) {
+        mainmenu.refresh();
+      }
+      //game = new Game("Duber's Revenge");
+      while (running) {
+        game.refresh();
+/*        if(running==false) {
+          System.out.println("You died");
+          game.ending();
+          while(end) game.refresh2();
+        }*/
+      }
+      System.out.println("You died");
+     // game.ending();
+      while(end) game.refresh();
+
+      if(!menu) System.exit(0);
+    }
+  }
+
+}
