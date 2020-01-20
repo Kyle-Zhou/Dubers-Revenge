d2773cd793bbcebd7134e59ba1d0b6af981e01b4
Meow157 <57512426+MiaoE@users.noreply.github.com>
2020-01-20 02:54:53 -0500

    Update Main.java

--- 2020.1.20.0/Main.java
+++ 2020.1.20.0/Main.java
@@ -1,40 +1,44 @@
-/*
-* Main.java
-* Editor: Eric Miao
-* January 20, 2020
- */
-
-public class Main {
+/**
+ * [Main.java]
+ * The Main class of the project
+ * @author Eric Miao
+ **/
+public class Main{
 
   public static boolean menu = true, running = false, end = false;
   public static Game game;
 
-  public static void main(String[] args) {
+  /**main
+   * the main method is required in all projects
+   * @param args, String array arguments
+   */
+  public static void main(String[] args){
 
-    while (true) {
+    while (true){
       Menu mainmenu;
-      if(menu) { // when game repeats itself there's a possibility that it may not redirect to menu
+      if(menu){ // when game repeats itself there's a possibility that it may not redirect to menu
         mainmenu = new Menu(); // creates menu Frame
-        while (menu) { // while loop for menu Frame
+        while (menu){ // while loop for menu Frame
           mainmenu.refresh(); // awaiting commands...
         }
       }
 
-      if(running) { // check if game is running
+      if(running){ // check if game is running
         start(); //create a new game
-        while (running) { //Main Loop
+        while (running){ //Main Loop
           game.refresh();
         }
       }
-
-      //Oh no you died
-      //System.out.println("Died");
 
       while(end) game.refresh(); //a different boolean variable assists the KeyListener
     }
   }
 
-  private static void start() { // start a new game
+  /**start
+   * A static method that creates a new Game Frame
+   * Method is used before a new Game starts
+   */
+  private static void start(){ // start a new game
     game = new Game("Duber's Revenge");
   }
 }
