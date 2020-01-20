06bcb56cac2a9c80cff7d83acea663cf3a1d349e
Meow157 <57512426+MiaoE@users.noreply.github.com>
2020-01-20 03:04:05 -0500

    Update Keys.java

--- 2020.1.20.0/Keys.java
+++ 2020.1.20.0/Keys.java
@@ -1,7 +1,10 @@
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.Graphics;
 
+/**[Keys.java]
+ * An extended class of KeyListener, gets any activities on the keyboard
+ * @author
+ */
 public class Keys implements KeyListener{
 
   private Human duber;
@@ -10,12 +13,19 @@ public class Keys implements KeyListener{
   private Spawner spawner;
   private HeadUpDisplay hud;
   private Shop shop;
-  private Graphics g;
   private static boolean left, down, right, up;
   private Counter counter;
 
-  Keys() {}
-
+  /**Keys constructor
+   * The constructor inherits Duber, Weapons, Spawner, HUD, entities, shope, and counter
+   * @param duber, Human object that represents Duber
+   * @param weapons, a 1D array that consists all the weapons in the game
+   * @param spawner, a spawner that spawns almost everything in game
+   * @param hud, the screen that follows wherever Duber goes
+   * @param entities, a 1D array that consists all the data and objects
+   * @param shop, an object that contains buttons to purchase all kinds of guns
+   * @param counter, a general counter used for Grace Periods and Reload
+   */
   Keys(Human duber, Weapon[] weapons, Spawner spawner, HeadUpDisplay hud, Entity entities[], Shop shop, Counter counter){
     this.duber = duber;
     this.weapons = weapons;
@@ -25,47 +35,55 @@ public class Keys implements KeyListener{
     this.shop = shop;
     this.counter = counter;
   }
-  
-  public void keyTyped(KeyEvent e) {
+
+  /**keyTyped
+   * A method used to detect if a key is pressed and immediately released
+   * @param e, an object used to detect any KeyEvent
+   */
+  public void keyTyped(KeyEvent e){
   }
 
-  public void keyPressed(KeyEvent e) {
-    if(Main.running) {
-      if (KeyEvent.getKeyText(e.getKeyCode()).equals("A")) {  //If 'A' is pressed
+  /**keyPressed
+   * A method used to detect if a key is pressed down
+   * @param e, an object used to detect any KeyEvent
+   */
+  public void keyPressed(KeyEvent e){
+    if(Main.running){
+      if (KeyEvent.getKeyText(e.getKeyCode()).equals("A")){  //If 'A' is pressed
         left = true;
         duber.setxDirection(-duber.getSpeed());
       } else if (KeyEvent.getKeyText(e.getKeyCode()).equals("S")){  //If 'S' is pressed
         down = true;
         duber.setyDirection(duber.getSpeed());
-      } else if (KeyEvent.getKeyText(e.getKeyCode()).equals("D")) {  //If 'D' is pressed
+      } else if (KeyEvent.getKeyText(e.getKeyCode()).equals("D")){  //If 'D' is pressed
         right = true;
         duber.setxDirection(duber.getSpeed());
-      } else if (KeyEvent.getKeyText(e.getKeyCode()).equals("W")) { //If 'W' is pressed
+      } else if (KeyEvent.getKeyText(e.getKeyCode()).equals("W")){ //If 'W' is pressed
         up = true;
         duber.setyDirection(-duber.getSpeed());
       }
 
-      if ((e.getKeyChar() == '1')) {
+      if ((e.getKeyChar() == '1')){
         spawner.setCurrentWeapon(0);
-      } else if ((e.getKeyChar() == '2')) {
+      } else if ((e.getKeyChar() == '2')){
         if (shop.getShotgunBought()){
           spawner.setCurrentWeapon(1);
         }
-      } else if (e.getKeyChar() == '3') {
-        if (shop.getAutomaticBought()) {
+      } else if (e.getKeyChar() == '3'){
+        if (shop.getAutomaticBought()){
           spawner.setCurrentWeapon(2);
         }
-      } else if (e.getKeyChar() == '4') {
-        if (shop.getRocketLauncherBought()) {
+      } else if (e.getKeyChar() == '4'){
+        if (shop.getRocketLauncherBought()){
           spawner.setCurrentWeapon(3);
         }
-      } else if (e.getKeyChar() == '5') {
-        if (shop.getFlameThrowerBought()) {
+      } else if (e.getKeyChar() == '5'){
+        if (shop.getFlameThrowerBought()){
           spawner.setCurrentWeapon(4);
         }
       }
 
-      if (KeyEvent.getKeyText(e.getKeyCode()).equals("E")) {
+      if (KeyEvent.getKeyText(e.getKeyCode()).equals("E")){
         if (entities[spawner.getCurrentBandage()] instanceof Bandage){
           if (entities[spawner.getCurrentBandage()].getHitbox().intersects(duber.getHitbox())){
             hud.setBandageCount(hud.getBandageCount()+1);
@@ -81,7 +99,7 @@ public class Keys implements KeyListener{
         }
       }
 
-      if (KeyEvent.getKeyText(e.getKeyCode()).equals("R")) {
+      if (KeyEvent.getKeyText(e.getKeyCode()).equals("R")){
         if (weapons[spawner.getCurrentWeapon()].getClip() != weapons[spawner.getCurrentWeapon()].getOriginalClip()){
           counter.setReloadTime(System.currentTimeMillis());
           weapons[spawner.getCurrentWeapon()].setReloading(true);
@@ -89,18 +107,18 @@ public class Keys implements KeyListener{
         }
       }
 
-      if (KeyEvent.getKeyText(e.getKeyCode()).equals("H")) {
+      if (KeyEvent.getKeyText(e.getKeyCode()).equals("H")){
         if (hud.getBandageCount() > 0){
           hud.setBandageCount(hud.getBandageCount()-1);
           if (duber.getHealth() + 20 > 100){
             duber.setHealth(100);
-          } else {
+          } else{
             duber.setHealth(duber.getHealth() + 20);
           }
         }
       }
 
-      if (KeyEvent.getKeyText(e.getKeyCode()).equals("J")) {
+      if (KeyEvent.getKeyText(e.getKeyCode()).equals("J")){
         if (hud.getMedKitCount() > 0){
           hud.setMedKitCount(hud.getMedKitCount()-1);
           duber.setHealth(100);
@@ -114,30 +132,34 @@ public class Keys implements KeyListener{
           weapons[spawner.getCurrentWeapon()].setShootingLock(false);
         }
       }
-    }else { //only Main.end == true will use this
-      if (KeyEvent.getKeyText(e.getKeyCode()).equals("R")) {
-        Main.game.dispose();
+    }else{ //only Main.end == true will use this
+      if (KeyEvent.getKeyText(e.getKeyCode()).equals("R")){
         Main.end = false;
         Main.menu = false;
         Main.running = true;
-        System.out.println("Starting new Game");
-      } else if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
         Main.game.dispose();
+        System.out.println("Starting new Game");
+      } else if (e.getKeyCode() == KeyEvent.VK_ESCAPE){
         Main.end = false;
         Main.running = false;
         Main.menu = true;
+        Main.game.dispose();
       }
     }
   }
 
-  public void keyReleased(KeyEvent e) {
-    if (KeyEvent.getKeyText(e.getKeyCode()).equals("A")) {    //Good time to use a Switch statement
+  /**keyReleased
+   * A method used to detect when the key is released
+   * @param e, an object used to detect any KeyEvent
+   */
+  public void keyReleased(KeyEvent e){
+    if (KeyEvent.getKeyText(e.getKeyCode()).equals("A")){    //Good time to use a Switch statement
       duber.setxDirection(0);
       left = false;
-    } else if (KeyEvent.getKeyText(e.getKeyCode()).equals("S")) {
+    } else if (KeyEvent.getKeyText(e.getKeyCode()).equals("S")){
       duber.setyDirection(0);
       down = false;
-    } else if (KeyEvent.getKeyText(e.getKeyCode()).equals("D")) {
+    } else if (KeyEvent.getKeyText(e.getKeyCode()).equals("D")){
       duber.setxDirection(0);
       right = false;
     } else if (KeyEvent.getKeyText(e.getKeyCode()).equals("W")){
@@ -145,11 +167,16 @@ public class Keys implements KeyListener{
       up = false;
     }  //note - would be better to make player class and pass in map, test movement in there
   }
-  
-  public boolean movement() {
-    if (left || down || right || up) {
+
+  /**movement
+   * A method used to detect if the character(Duber) is moving or not
+   * Method returns value based on the Key Activity
+   * @return true if any key is pressed or held, false if no activity on keyboard
+   */
+  public boolean movement(){
+    if (left || down || right || up){
       return true;
-    } else {
+    } else{
       return false;
     }
   }
